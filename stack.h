d43793acf7bfd53918ddbf0d50a00e464ca0548b
Kirvvs <jb.ursua13@gmail.com>
2023-02-26 22:21:32 +0800

    stacks v1.0

--- stack.h
+++ stack.h
@@ -2,6 +2,7 @@
 YOU ARE NOT ALLOWED TO MODIFY THE STRUCT AND THE FUNCTION PROTOTYPES
 *******************************************************************/
 
+#include "stdio.h"
 #include "stdlib.h"
 
 #include "node.h"
@@ -17,6 +18,7 @@ stack* createStack(int n) {
 	stacky->n = n;
 	stacky->nCount = 0;
 	stacky->pTop = NULL;
+	printf("A Stack has been created with %d slots!\n\n", n);
 	return stacky;
 }
 
@@ -30,32 +32,74 @@ int stackFull(stack *s) {
 	else return 0;
 }
 
-void push(stack **s, char *data) {
-	sNode *sNewNode = malloc(sizeof(sNode));
-	char *dat = malloc(sizeof(char));
-	*dat = *data;
-	sNewNode->data = dat;
-	if((*s)->nCount != 0){
-		sNewNode->pLink = (*s)->pTop;
+void displayStack(stack *s) {
+	sNode *iNode;
+	if(!stackEmpty(s)){
+		for(int i = 1; i <= s->n; i++){
+			iNode = s->pTop;
+			if(i <= s->nCount){
+				for(int j = s->nCount; j > i; j--){
+					iNode = iNode->pLink;
+				}
+				printf("%c ", *(iNode->data));
+			}
+			else{
+				printf("□ ");
+			}
+		}
+		printf("\n");
+		for(int i = 1; i <= s->n; i++){
+			if(i == s->nCount) printf("△ ");
+			else printf("  ");
+		}
+		printf("\n\n");
 	}
-	(*s)->pTop = sNewNode;
-	
-	++(*s)->nCount;
+	else{
+		printf("Nothing to display...\n\n");
+	}
+}
+
+void push(stack **s, char *data) {
+	if(!stackFull(*s)){
+		sNode *sNewNode = malloc(sizeof(sNode));
+
+		char *dat = malloc(sizeof(char));
+		*dat = *data;
+		
+		sNewNode->data = dat;
+		if((*s)->nCount != 0){
+			sNewNode->pLink = (*s)->pTop;
+		}
+		(*s)->pTop = sNewNode;
+		
+		++(*s)->nCount;
+		printf("[+] Pushed \"%c\" to stack!\n", *data);
+		displayStack(*s);
+	}
+	else{
+		printf("Stack is currenty full!\n");
+	}
 }
 
 char* pop(stack **s) {
-	char *data = (*s)->pTop->data;
+	if(!stackEmpty(*s)){
+		char *data = (*s)->pTop->data;
 
-	(*s)->pTop = (*s)->pTop->pLink;
+		(*s)->pTop = (*s)->pTop->pLink;
 
-	--(*s)->nCount;
-	return data;
-}
-
-void displayStack(stack *s) {
-	// Your code here
+		--(*s)->nCount;
+		printf("[-] Popped stack and returned \"%c\"\n", *data);
+		displayStack(*s);
+		return data;
+	}
+	else{
+		printf("Stack is currently empty!");
+		return NULL;
+	}
+	
 }
 
 char* top(stack *s) {
+	printf("Current Top Data of Stack: %c\n\n", *(s->pTop->data));
 	return s->pTop->data;
 }
