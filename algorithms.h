aeeed0d9f7f859eac7429b4545bc9857ae6c9bc7
ALTelesto <83495459+ALTelesto@users.noreply.github.com>
2023-03-10 00:33:26 +0800

    almost finished, just need to rewrite to eliminate garbo values

--- algorithms.h
+++ algorithms.h
@@ -6,20 +6,6 @@ YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
 #include "string.h"
 #include "stdlib.h"
 
-void convertToken(char *infix, char *postfix){
-
-}
-
-int getLen(char *infix, int i){
-	char cTemp;
-	int j = 0;
-	do{
-		cTemp = *(infix+i+j);
-		j += 1;
-	}while(cTemp != ' ' && cTemp != '\0');
-	return j;
-}
-
 int checkOperand(char *strTemp){
 	if(strcmp(strTemp,"(") == 0){
 		return 0;
@@ -42,33 +28,33 @@ int checkOperand(char *strTemp){
 	return -1;
 }
 
-int isHigherOperand(int nOp, stack *sStack){
-	if(top(sStack) == NULL){
+int isHigherOperand(int nOp, stack *pStack){
+	if(top(pStack) == NULL){
 		return 0;
 	}
-	int nTopOp = checkOperand(top(sStack));
+	int nTopOp = checkOperand(top(pStack));
 	if(nOp <= nTopOp){
 		return 1;
 	}
 	return 0;
 }
 
-void popOp(int nOp, stack *sStack, char *postfix){
-	int nGo = checkOperand(top(sStack));
+void popOp(int nOp, stack *pStack, char *postfix){
+	int nGo = checkOperand(top(pStack));
 	char strTemp[3];
 	while(nOp<=nGo){
-		if(checkOperand(top(sStack)) == 0 || checkOperand(top(sStack)) == 5){
-				pop(&sStack);
+		if(checkOperand(top(pStack)) == 0 || checkOperand(top(pStack)) == 5){
+				pop(&pStack);
 		}
 		else{
-			strcpy(strTemp,pop(&sStack));
+			strcpy(strTemp,pop(&pStack));
 			strcat(strTemp, " ");
 			strcat(postfix,strTemp);
 		}
 		
-		nGo = isHigherOperand(nOp, sStack);
+		nGo = isHigherOperand(nOp, pStack);
 
-		if(top(sStack) == NULL){
+		if(top(pStack) == NULL){
 			nGo = -1;
 		}
 	}
@@ -76,45 +62,50 @@ void popOp(int nOp, stack *sStack, char *postfix){
 
 void infixToPostfix(char *infix, char *postfix) {
 	// Your code here
-	stack* sStack = createStack(10);
+	stack* pStack = createStack(10);
 	int nOp;
-	char strTemp[256];
+	char *strTemp = malloc(sizeof(char)+257);
 
-	int i;
+	int i,j;
 	for(i = 0; i<=strlen(infix); i++){
 		if(*(infix+i) != ' ' && *(infix+i) != '\0'){
+			printf("%s",infix+i);
 			strncat(strTemp,infix+i,1);
+			j++;
 			//("%d, %s\n",i,strTemp);
 		}
 		else if(*(infix+i) == ' '){
 			//failsafe if strTemp is empty for some reason
+			strTemp[j]='\0';
+			j=0;
 			if(strTemp[0]!='\0'){
 				nOp = checkOperand(strTemp);
 				//printf("\n%d\n",nOp);
+				printf("\n%s\n",strTemp);
 				switch(nOp){
 					case 0:
-						push(&sStack,strTemp);
+						push(&pStack,strTemp);
 						break;
 					case 1:case 2:case 3:case 4:
 						//
-						if(isHigherOperand(nOp,sStack) == 1){
-							popOp(nOp,sStack,postfix);
-							push(&sStack,strTemp);
+						if(isHigherOperand(nOp,pStack) == 1){
+							popOp(nOp,pStack,postfix);
+							push(&pStack,strTemp);
 						}
 						else{
-							push(&sStack,strTemp);
+							push(&pStack,strTemp);
 						}
 						break;
 					case 5:
-						pop(&sStack);
-						popOp(1,sStack,postfix);
+						pop(&pStack);
+						popOp(1,pStack,postfix);
 						break;
 					default:
 						strcat(strTemp," ");
 						strcat(postfix, strTemp);
 						break;
 				}
-				//printf("\n%d, %s, postfix  %s\n\n",i,strTemp,postfix);
+				printf("\n%d, %s, postfix  %s\n\n",i,strTemp,postfix);
 				strTemp[0] = '\0';
 			}
 		}
@@ -122,10 +113,153 @@ void infixToPostfix(char *infix, char *postfix) {
 	if(strcmp(strTemp,")") != 0){
 		strcat(postfix, strTemp);
 	}
-	popOp(0,sStack,postfix);
+	popOp(0,pStack,postfix);
 }
 
 int evaluatePostfix(char *postfix) {
 	// Your code here
-	return 0;
+	stack* pStack = createStack(10);
+	int i,n1,n2;
+	char strTemp[256];
+	char *empty;
+	
+	for(i = 0; i<=strlen(postfix); i++){
+		if(*(postfix+i) != ' ' && *(postfix+i) != '\0'){
+			strncat(strTemp,postfix+i,1);
+			printf("strTemp update: %s\n",strTemp);
+		}
+		else if(*(postfix+i) == ' '){
+			//failsafe if strTemp is empty for some reason
+			printf("strTemp is %s\n\n",strTemp);
+			if(strTemp[0]!='\0'){
+				if(strcmp(strTemp,"+") == 0){
+					n2 = (int)strtol(pop(&pStack),&empty,10);
+					n1 = (int)strtol(pop(&pStack),&empty,10);
+					sprintf(strTemp,"%d",n1+n2);
+					push(&pStack,strTemp);
+				}
+				else if(strcmp(strTemp,"-") == 0){
+					n2 = (int)strtol(pop(&pStack),&empty,10);
+					n1 = (int)strtol(pop(&pStack),&empty,10);
+					sprintf(strTemp,"%d",n1-n2);
+					push(&pStack,strTemp);
+				}
+				else if(strcmp(strTemp,"/") == 0){
+					n2 = (int)strtol(pop(&pStack),&empty,10);
+					n1 = (int)strtol(pop(&pStack),&empty,10);
+					sprintf(strTemp,"%d",n1/n2);
+					push(&pStack,strTemp);
+				}
+				else if(strcmp(strTemp,"*") == 0){
+					n2 = (int)strtol(pop(&pStack),&empty,10);
+					n1 = (int)strtol(pop(&pStack),&empty,10);
+					sprintf(strTemp,"%d",n1*n2);
+					push(&pStack,strTemp);
+				}
+				else if(strcmp(strTemp,"!") == 0){
+					n1 = (int)strtol(pop(&pStack),&empty,10);
+					if(n1 == 0){
+						sprintf(strTemp,"%d",1);
+					}
+					else{
+						sprintf(strTemp,"%d",0);
+					}
+					push(&pStack,strTemp);
+				}
+				else if(strcmp(strTemp,"&&") == 0){
+					n2 = (int)strtol(pop(&pStack),&empty,10);
+					n1 = (int)strtol(pop(&pStack),&empty,10);
+					if(n1 != 0 && n2 != 0){
+						sprintf(strTemp,"%d",1);
+					}
+					else{
+						sprintf(strTemp,"%d",0);
+					}
+					push(&pStack,strTemp);
+				}
+				else if(strcmp(strTemp,"||") == 0){
+					n2 = (int)strtol(pop(&pStack),&empty,10);
+					n1 = (int)strtol(pop(&pStack),&empty,10);
+					if(n1 != 0 || n2 != 0){
+						sprintf(strTemp,"%d",1);
+					}
+					else{
+						sprintf(strTemp,"%d",0);
+					}
+					push(&pStack,strTemp);
+				}
+				else if(strcmp(strTemp,">") == 0){
+					n2 = (int)strtol(pop(&pStack),&empty,10);
+					n1 = (int)strtol(pop(&pStack),&empty,10);
+					if(n1 > n2){
+						sprintf(strTemp,"%d",1);
+					}
+					else{
+						sprintf(strTemp,"%d",0);
+					}
+					push(&pStack,strTemp);
+				}
+				else if(strcmp(strTemp,"<") == 0){
+					n2 = (int)strtol(pop(&pStack),&empty,10);
+					n1 = (int)strtol(pop(&pStack),&empty,10);
+					if(n1 < n2){
+						sprintf(strTemp,"%d",1);
+					}
+					else{
+						sprintf(strTemp,"%d",0);
+					}
+					push(&pStack,strTemp);
+				}
+				else if(strcmp(strTemp,">=") == 0){
+					n2 = (int)strtol(pop(&pStack),&empty,10);
+					n1 = (int)strtol(pop(&pStack),&empty,10);
+					if(n1 >= n2){
+						sprintf(strTemp,"%d",1);
+					}
+					else{
+						sprintf(strTemp,"%d",0);
+					}
+					push(&pStack,strTemp);
+				}
+				else if(strcmp(strTemp,"<=") == 0){
+					n2 = (int)strtol(pop(&pStack),&empty,10);
+					n1 = (int)strtol(pop(&pStack),&empty,10);
+					if(n1 <= n2){
+						sprintf(strTemp,"%d",1);
+					}
+					else{
+						sprintf(strTemp,"%d",0);
+					}
+					push(&pStack,strTemp);
+				}
+				else if(strcmp(strTemp,"!=") == 0){
+					n2 = (int)strtol(pop(&pStack),&empty,10);
+					n1 = (int)strtol(pop(&pStack),&empty,10);
+					if(n1 != n2){
+						sprintf(strTemp,"%d",1);
+					}
+					else{
+						sprintf(strTemp,"%d",0);
+					}
+					push(&pStack,strTemp);
+				}
+				else if(strcmp(strTemp,"==") == 0){
+					n2 = (int)strtol(pop(&pStack),&empty,10);
+					n1 = (int)strtol(pop(&pStack),&empty,10);
+					if(n1 == n2){
+						sprintf(strTemp,"%d",1);
+					}
+					else{
+						sprintf(strTemp,"%d",0);
+					}
+					push(&pStack,strTemp);
+				}
+				else{
+					push(&pStack,strTemp);
+				}
+			}
+		}
+	}
+
+	return (int)strtol(pop(&pStack),&empty,10);
 }
